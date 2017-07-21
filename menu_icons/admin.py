fe94594154f80672cd21ce5172bfe21ef3289dc1
np <np@exemple.com>
2017-07-21 16:05:23 +0200

    Add frontend editing Menu Icon

--- menu_icons/admin.py
+++ menu_icons/admin.py
@@ -1,15 +1,16 @@
+from cms.admin.placeholderadmin import FrontendEditableAdminMixin
 from django.contrib import admin
 from cms.extensions import PageExtensionAdmin
 from menu_icons.models import MenuIcon
 
 
-class MenuIconAdmin(PageExtensionAdmin):
+class MenuIconAdmin(FrontendEditableAdminMixin,PageExtensionAdmin,admin.ModelAdmin):
     """
     Adds fields for Menu Icons
     """
     fieldsets = [
         ('Menu Icons - django CMS extension (USELESS IN THE BACKEND)', {
-            'fields': ['menu_icon_image', 'menu_icon_url', 'menu_icon_font_awesome']
+            'fields': ['menu_icon_image', 'menu_icon_url', 'menu_icon_font_awesome' ,'menu_class_for_link_icon']
         }),
     ]
 
